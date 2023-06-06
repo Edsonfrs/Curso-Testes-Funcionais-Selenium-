c393cb7829dce846289c72408de7f9c00c3e50b2
Edsonfrs <edsonfrs@gmail.com>
2023-06-06 11:29:33 -0300

    Elementos: Frames/Windows/WindowHandler/WindowHandlers

---
+++ src/test/java/br/com/edsonfrs/automacao/FrameAndWindowTest.java
@@ -0,0 +1,63 @@
+package br.com.edsonfrs.automacao;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FrameAndWindowTest {
+    @Test
+    public void deveDeveInteragirComElementosDentroDeUmFrame() {
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("file:///"+System.getProperty("user.dir")+"/camp/componentes.html");
+
+        driver.switchTo().frame("frame1");
+        driver.findElement(By.id("frameButton")).click();
+        Alert alert = driver.switchTo().alert();
+        String texto = alert.getText();
+        System.out.println(texto);
+        Assertions.assertEquals("Frame OK!", texto);
+        alert.accept();
+        driver.switchTo().defaultContent();
+        driver.findElement(By.id("elementosForm:nome")).sendKeys(texto);
+
+        driver.quit();
+    }
+
+    @Test
+    public void deveDeveInteragirComElementosDentroDeUmaJanela() {
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("file:///"+System.getProperty("user.dir")+"/camp/componentes.html");
+
+        driver.findElement(By.id("buttonPopUpEasy")).click();
+        driver.switchTo().window("Popup");
+        driver.findElement(By.tagName("textarea")).sendKeys("Ser ou não ser... ");
+        driver.close();
+        driver.switchTo().window("");
+        driver.findElement(By.tagName("textarea")).sendKeys("... eis a questão.");
+
+        driver.quit();
+    }
+
+    @Test
+    public void deveDeveInteragirComElementosDentroDeUmaJanelaSemTitulo() {
+        System.setProperty("webdriver.chrome.driver","drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("file:///"+System.getProperty("user.dir")+"/camp/componentes.html");
+
+        driver.findElement(By.id("buttonPopUpHard")).click();
+        //System.out.println(driver.getWindowHandle());
+        //System.out.println(driver.getWindowHandles());
+        driver.switchTo().window((String) driver.getWindowHandles().toArray()[1]);
+        driver.findElement(By.tagName("textarea")).sendKeys("Ser ou não ser... novamente ");
+        driver.close();
+        driver.switchTo().window((String) driver.getWindowHandles().toArray()[0]);
+        driver.findElement(By.tagName("textarea")).sendKeys("... eis a questão... novamente.");
+
+        driver.quit();
+    }
+}
